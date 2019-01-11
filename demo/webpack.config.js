6062bb4239de49c533a1a2109ad78efd22bd836e
vijayst <vijayst@gmail.com>
2019-01-12 00:54:08 +0530

    add redirects

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -1,10 +1,14 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const CopyWebPackPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
 const htmlWebpackPlugin = new HtmlWebPackPlugin({
     template: './index.html',
     filename: './index.html'
 });
+const copyWebPackPlugin = new CopyWebPackPlugin([{
+    from: '_redirects'
+}]);
 
 module.exports = {
     mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
@@ -30,5 +34,5 @@ module.exports = {
             }
         ]
     },
-    plugins: [htmlWebpackPlugin]
+    plugins: [htmlWebpackPlugin, copyWebPackPlugin]
 };
