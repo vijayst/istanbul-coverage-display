12dfd484765245bfa0d2a91a32bd2698d95d3b2f
vijayst <vijayst@gmail.com>
2019-01-11 07:54:55 +0530

    trimmed getCoverage fn

--- src/getCoverage.js
+++ src/getCoverage.js
@@ -4,18 +4,10 @@ export default function getCoverage() {
     if (window.__coverage__ && libCoverage) {
         var map = libCoverage.createCoverageMap({});
         map.merge(window.__coverage__);
-        let minSlashes = 100;
-        const fileCoverages = Object.keys(map.data).map(key => {
-            const slashes = key.split('/').length;
-            if (slashes < minSlashes) {
-                minSlashes = slashes;
-            }
-            return {
-                key,
-                data: map.data[key].toSummary().data
-            };
-        });
-        return fileCoverages;
+        return Object.keys(map.data).map(key => ({
+            key,
+            data: map.data[key].toSummary().data
+        }));
     }
     return null;
 }
