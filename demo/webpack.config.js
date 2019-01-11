832c1ce32a344ea1ef0dfa0daa883c743c3b915b
vijayst <vijayst@gmail.com>
2019-01-12 01:06:32 +0530

    fix webpack config

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -15,7 +15,8 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve('dist'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        publicPath: '/'
     },
     module: {
         rules: [
